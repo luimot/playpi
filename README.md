d7b3cba088abb69fff95cb1143f1f63a1f624172
luimot <luiz.mota1999@usp.br>
2020-06-22 01:45:30 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-playpi
+# playpi
+
+Um aplicativo pra testar conectividade Bluetooth com Raspberry Pi
+
+## Eu tava entediado
---
+++ lib/connect.dart
@@ -0,0 +1,34 @@
+/*  Links importantes
+Package Bluetooth:
+  Pacote			-https://pub.dev/packages/flutter_blue    ##ESTÁ EM VERSÃO ALPHA POR ENQUANTO###
+  Repositório -https://github.com/pauldemarco/flutter_blue
+	Plug-in:		-https://medium.com/flutter-comunidade-br/utilizando-bluetooth-no-flutter-3e1c480f0224
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+class Conecta extends StatefulWidget {
+  @override
+  _ConectaState createState() => _ConectaState();
+}
+
+class _ConectaState extends State<Conecta> {
+  FlutterBlue fBlue = FlutterBlue.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("_Parear Dispositivo"),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child:Text("Procurar"),
+        onPressed: (){
+          //TODO: IMPLEMENTAR BUSCA NO FLUTTERBLUE
+        }
+      ),
+    );
+  }
+}
